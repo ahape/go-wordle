489346e0760e741c6582dd3c9d2bdedf1d439fdb
Alan Hape <epalan@gmail.com>
2022-04-08 23:49:03 -0700

    Fix path bug

--- getjson.go
+++ getjson.go
@@ -6,7 +6,7 @@ import (
 )
 
 func getJson() Config {
-    file, _ := ioutil.ReadFile("../words.json")
+    file, _ := ioutil.ReadFile("./words.json")
     var parsed Config
     _ = json.Unmarshal(file, &parsed)
     parsed["allWords"] = append(parsed["answers"], parsed["others"]...)
